a8c312b7b7a1b3eb7069f300f6c3eff8f0c5bd8e
Dongkyun_Jang <thd6576@naver.com>
2021-09-10 16:23:05 +0900

    feat: add files for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { createApp } from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { store } from './store'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { store } from "./store";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-library.add(fas)
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+  "GET,PUT,POST,DELETE,PATCH,OPTIONS";
+axios.defaults.headers.common["Access-Control-Allow-Headers"] =
+  "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization";
+
+library.add(fas);
 
 createApp(App)
-    .use(store)
-    .use(router)
-    .component('fa', FontAwesomeIcon)
-    .mount('#app')
+  .use(store)
+  .use(router)
+  .component("fa", FontAwesomeIcon)
+  .mount("#app");
