2bb8655d7a4fed8348c75ca29bd0bd17103b719d
Dongkyun_Jang <thd6576@naver.com>
2021-09-13 21:20:03 +0900

    fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ import axios from "axios";
 
 // axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers.common["Access-Control-Allow-Methods"] =
-  "GET,PUT,POST,DELETE,PATCH,OPTIONS";
+// axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+// "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 axios.defaults.headers.common["Access-Control-Allow-Headers"] =
   "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization";
 
