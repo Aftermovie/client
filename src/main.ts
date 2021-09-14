b89991b1840f43c68daf011d7ec4bd5380073499
Dongkyun_Jang <thd6576@naver.com>
2021-09-14 09:47:09 +0900

    fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-// axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-// axios.defaults.headers.common["Access-Control-Allow-Methods"] =
-// "GET,PUT,POST,DELETE,PATCH,OPTIONS";
+axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+  "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 axios.defaults.headers.common["Access-Control-Allow-Headers"] =
   "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization";
 
